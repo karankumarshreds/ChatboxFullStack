8b2ea4f29cc2052f709eab2d1c7052a6dee103df
Karan Kumar <nailedITnerd@gmail.com>
2020-08-15 13:30:11 +0530

    State bug

--- client/pages/chat.js
+++ client/pages/chat.js
@@ -5,18 +5,15 @@ import { useContext } from 'react';
 
 let socket;
 
-const Chat = ({ karan }) => {
-    const backendEndpoint = 'http://localhost:5000';
+const Chat = () => {
+    // const backendEndpoint = 'http://localhost:5000';
     const [state, dispatch] = useContext(DataContext);
-    useEffect(() => {
-        socket = io(backendEndpoint);
-    }, [backendEndpoint])
-
+    console.log('STATE', state)
     return <h1>{karan}</h1>
 }
 
 Chat.getInitialProps = async (ctx) => {
-    return { karan: 'karan' }
+    return {}
 }
 
 export default Chat;
